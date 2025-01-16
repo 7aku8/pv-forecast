48240eb19d08c83d5f49e936665a1fb78a0da395
7aku8 <jakub.wolak.dev@gmail.com>
2025-01-16 21:36:25 +0100

    add chart

--- lib/dashboard/view/dashboard_page.dart
+++ lib/dashboard/view/dashboard_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pv_forecast/components/bottom_nav.dart';
+import 'package:pv_forecast/components/main_chart.dart';
 import 'package:pv_forecast/components/top_bar.dart';
 import 'package:pv_forecast/utils/colors.dart';
 
@@ -16,7 +17,11 @@ class _DashboardPageState extends State<DashboardPage> {
     return const Scaffold(
       backgroundColor: backgroundColor,
       appBar: TopBar(),
-      body: Center(child: Text('dupa')),
+      body: Column(
+        children: [
+          MainChart(),
+        ],
+      ),
       bottomNavigationBar: BottomNav(),
     );
   }
