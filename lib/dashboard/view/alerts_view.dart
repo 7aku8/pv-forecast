a174622b98af5b997f8a21e68f86519731a3a58a
7aku8 <jakub.wolak.dev@gmail.com>
2025-01-21 20:48:40 +0100

    change alerts styles

--- lib/dashboard/view/alerts_view.dart
+++ lib/dashboard/view/alerts_view.dart
@@ -35,6 +35,13 @@ class _HomeViewState extends State<AlertsView> {
             return const SizedBox(height: 64);
           }
 
+          if (index == 0) {
+            return Padding(
+              padding: const EdgeInsets.only(top: 8),
+              child: _renderAlarm(alerts[index]),
+            );
+          }
+
           return _renderAlarm(alerts[index]);
         },
       ),
@@ -52,79 +59,38 @@ class _HomeViewState extends State<AlertsView> {
     }
   }
 
-  Widget _renderAlarm(Alert alert) {
-    if (alert.severity == Severity.info) {
-      return Padding(
-        padding: const EdgeInsets.only(right: 8),
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(24),
-            color: Colors.white,
-            border: Border.all(color: Colors.grey.shade200),
-          ),
-          padding: const EdgeInsets.only(bottom: 16),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 16),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      _getName(alert.severity),
-                      style: const TextStyle(
-                        fontWeight: FontWeight.w500,
-                        color: Colors.black87,
-                      ),
-                    ),
-                    const Spacer(),
-                    Text(alert.time),
-                    const SizedBox(width: 8),
-                    Transform.translate(
-                      offset: const Offset(8, -8),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(24),
-                          border: Border.all(
-                            color: Colors.grey.shade200,
-                          ),
-                        ),
-                        padding: const EdgeInsets.all(8),
-                        child: Icon(
-                          Icons.power,
-                          color: Colors.grey.shade300,
-                          size: 24,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 12),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: alert.message,
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      );
+  Color _getBgColor(Severity severity) {
+    switch (severity) {
+      case Severity.info:
+        return Colors.white!;
+      case Severity.warning:
+        return Colors.orange[50]!;
+      case Severity.error:
+        return Colors.red[50]!;
     }
+  }
 
+  Color _getBorderColor(Severity severity) {
+    switch (severity) {
+      case Severity.info:
+        return Colors.grey[200]!;
+      case Severity.warning:
+        return Colors.orange[100]!;
+      case Severity.error:
+        return Colors.red[100]!;
+    }
+  }
+
+  Widget _renderAlarm(Alert alert) {
     return Padding(
       padding: const EdgeInsets.only(right: 8),
       child: Container(
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(24),
-          color: alert.severity == Severity.warning
-              ? Colors.orange[100]
-              : Colors.red[200],
+          color: _getBgColor(alert.severity),
+          border: Border.all(
+            color: _getBorderColor(alert.severity),
+          ),
         ),
         padding: const EdgeInsets.only(bottom: 16),
         child: Column(
@@ -147,11 +113,18 @@ class _HomeViewState extends State<AlertsView> {
                   const SizedBox(width: 8),
                   Transform.translate(
                     offset: const Offset(8, -8),
-                    child: CircleAvatar(
-                      backgroundColor: Colors.black87,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(24),
+                        border: Border.all(
+                          color: Colors.grey.shade200,
+                        ),
+                      ),
+                      padding: const EdgeInsets.all(8),
                       child: Icon(
                         _getIcon(alert.severity),
-                        color: Colors.white,
+                        color: Colors.grey.shade300,
                         size: 24,
                       ),
                     ),
